a7b3acaa8863defae8c3ba059062881810b669e1
dredvard <ekambulow@gmail.com>
2017-12-27 17:03:20 -0500

    Revised initial commit

---
+++ desktop/src/com/kambul/flute/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kambul.flute.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kambul.flute.JoelFluteDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JoelFluteDemo(), config);
+	}
+}
